d8df1aee2bda843be7fd16b61ce560d8b61274b6
James Pilkington <jpilkington332@gmail.com>
2026-01-28 16:49:11 -0700

    vercel commit

---
+++ app/(app)/layout.tsx
@@ -0,0 +1,29 @@
+import { ClerkProvider } from "@clerk/nextjs";
+import { SanityLive } from "@/sanity/lib/live";
+import { CartStoreProvider } from "@/lib/store/cart-store-provider";
+import { Toaster } from "@/components/ui/sonner";
+import { Header } from "@/components/app/Header";
+import { ChatStoreProvider } from "@/lib/store/chat-store-provider";
+import { CartSheet } from "@/components/app/CartSheet";
+
+
+function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <ClerkProvider>
+      <CartStoreProvider>
+        <ChatStoreProvider>
+          {/* <AppShell> */}
+          <Header />
+          <main>{children}</main>
+          {/* </AppShell> */}
+          <CartSheet />
+          {/* <ChatSheet /> */}
+          <Toaster position="bottom-center" />
+          <SanityLive />
+        </ChatStoreProvider>
+      </CartStoreProvider>
+    </ClerkProvider >
+  );
+}
+
+export default Layout
